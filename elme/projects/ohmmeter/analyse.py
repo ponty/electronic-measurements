6b0e7cc8cdac8c1deeed1976f1e574395c5cc9cc
ponty <ponty@home>
2013-12-09 21:35:02 +0100

    ohmmeter: small improvements

--- elme/projects/ohmmeter/analyse.py
+++ elme/projects/ohmmeter/analyse.py
@@ -1,15 +1,15 @@
 from __future__ import division
 from elme.analyse import filter_measurements, calculate_ohm
+from elme.formating import format_ohmE12
+from uncertainties.unumpy.core import nominal_values
 
 
 def analyse(data):
-#     config = data.config
-
     m = filter_measurements(
         data.measurements,
         ['R'],
-#        ['Amiddle']
     )
     del m['t']
     m.insert(0, 'Rx', calculate_ohm(m.R, m.Atop, m.Amiddle, m.Abottom))
+    m.insert(0, 'Rx_E12', map(format_ohmE12, nominal_values(m.Rx)))
     return m
